ffe3266f17fb5275c5acdbc9f7b7fc992cb3b45a
symatt <jamesxsy17@gmail.com>
2022-11-15 20:03:34 +0800

    added comments

--- bulkImageEnhancer.py
+++ bulkImageEnhancer.py
@@ -6,7 +6,10 @@ import os
 import multiprocessing
 import time
 
+# enhances an image with the passed values
 def enhanceImage(uImg, terminate, shared_counter, semaphore, start, eTime, ePath, newBrightness, newSharpness, newContrast):
+    # acquires semaphore to check whether or not the current elapsed time is already greater than the time requirement
+    # terminates if current elapsed time is greater than the time req
     semaphore.acquire()
     if terminate.value:
         semaphore.release()
@@ -20,6 +23,7 @@ def enhanceImage(uImg, terminate, shared_counter, semaphore, start, eTime, ePath
         return
     semaphore.release()
 
+    # enhances image
     imgName = os.path.splitext(uImg[0])[0]
     fileExtension = os.path.splitext(uImg[0])[1]
 
@@ -34,12 +38,13 @@ def enhanceImage(uImg, terminate, shared_counter, semaphore, start, eTime, ePath
     newFileName = 'enhanced_' + imgName + fileExtension
     eImg.save(os.path.join(ePath, newFileName))
 
+    # acquires semaphore to increase the counter of saved enhanced images
     semaphore.acquire()
     shared_counter.value += 1
     print("saved " + imgName)
     semaphore.release()
 
-
+# gets the images and places it into an array [(name of img, img)]
 def getImages(uPath):
     uImages = []
 
@@ -50,28 +55,42 @@ def getImages(uPath):
         
     return uImages
 
+# creates manager and pool for multiprocessing
 def bulkImageEnchancer(uPath, ePath, eTime, newBrightness, newSharpness, newContrast, numProcess):
     print("NUMBER OF PROCESSES: " + str(numProcess))
     
+    # get time in seconds
     eTimeS = eTime * 60
 
+    # get images from unenhanced folder
     uImages = getImages(uPath)
 
+    # create manager
     with multiprocessing.Manager() as manager:
+        # create shared variables
         shared_counter = manager.Value('i', 0)
         terminate = manager.Value('t', False)
+        # create semaphore for accessing shared variables
         semaphore = manager.Semaphore(1)
+
         start = time.time()
         
+        # create pool with numProcess
         with multiprocessing.Pool(numProcess) as pool:
             items = [(img, terminate, shared_counter, semaphore, start, eTimeS, ePath, newBrightness, newSharpness, newContrast) for img in uImages]
+
+            # map all the items to the enhanceImage function and wait until it finishes
             res = pool.starmap_async(enhanceImage, items)
             res.wait()
+
+            # get elapsed time after all has finished or terminated early
             end = time.time()
             elapsed_time = (end - start) / 60
             print("Finished enhancing images")
             # print("total enhanced = " + str(shared_counter.value))
             # print("total elapsed time = " + str(elapsed_time))
+
+            # save information to text file
             f = open('results.txt', 'w')
             f.write("folder location: " + ePath +"\nnumber of images enhanced = " + str(shared_counter.value) + "\ntotal enhancing time = " + str(eTime) + "\ntotal elapsed time = " + str(elapsed_time))
             f.close()
