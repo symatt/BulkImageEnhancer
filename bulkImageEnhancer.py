1b8c58f48e7d31946ba673bb0bdca8d0a4d89e9b
symatt <jamesxsy17@gmail.com>
2022-11-15 11:23:56 +0800

    added multiprocessing, no locks yet

--- bulkImageEnhancer.py
+++ bulkImageEnhancer.py
@@ -1,6 +1,9 @@
+# https://superfastpython.com/multiprocessing-pool-for-loop/
+# https://superfastpython.com/multiprocessing-pool-mutex-lock/
 from PIL import Image
 from PIL import ImageEnhance
 import os
+import multiprocessing
 
 def enhanceImage(uImg, ePath, newBrightness, newSharpness, newContrast):
     imgName = os.path.splitext(uImg[0])[0]
@@ -18,17 +21,28 @@ def enhanceImage(uImg, ePath, newBrightness, newSharpness, newContrast):
 
 def getImages(uPath):
     uImages = []
+    count = 0
     for images in os.listdir(uPath):
-        if (images.endswith(".png") or images.endswith(".jpg") or images.endswith(".jpeg")):
+        if (images.endswith(".png") or images.endswith(".jpg") or images.endswith(".jpeg") or images.endswith(".gif")):
             img = Image.open(os.path.join(uPath, images))
             uImages.append((str(images),img))
-    return uImages
+            count += 1
+    return uImages, count
 
-def bulkImageEnchancer(uPath, ePath, eTime, newBrightness, newSharpness, newContrast):
-    uImages = getImages(uPath)
+def bulkImageEnchancer(uPath, ePath, eTime, newBrightness, newSharpness, newContrast, numProcess):
+    pool = multiprocessing.Pool(numProcess)
 
-    for img in uImages:
-        enhanceImage(img, ePath, newBrightness, newSharpness, newContrast)
+    uImages, count = getImages(uPath)
 
-bulkImageEnchancer(r'..\BulkImageEnhancer\unenhanced', r'..\BulkImageEnhancer\enhanced', 10, 10, 10, 10)
+    ePath = [ePath] * count
+    newBrightness = [newBrightness] * count
+    newSharpness = [newSharpness] * count
+    newContrast = [newContrast] * count
+
+    with multiprocessing.Pool() as pool:
+        pool.starmap(enhanceImage, zip(uImages, ePath, newBrightness, newSharpness, newContrast))
+    
+
+if __name__ == '__main__':
+    bulkImageEnchancer(r'..\BulkImageEnhancer\unenhanced', r'..\BulkImageEnhancer\enhanced', 10, 2, 3, 4, 5)
 
